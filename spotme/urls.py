8fab6a6ee18dfe279fe21a8248b16bac6c576bb6
Samuel Venzi <samuel.venzi@me.com>
2020-04-21 10:39:02 -0300

    Render SpotMe title

--- spotme/urls.py
+++ spotme/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from mainApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello_world.urls')),
+    path('', views.home, name='home'),
 ]
